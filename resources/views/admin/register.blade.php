85a98cfe7f6293488ebcc0dd9b2e4c1c7130a841
SoffiaNoor <soffianaisyah0@gmail.com>
2023-11-25 10:17:30 +0700

    *add

--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -14,10 +14,10 @@
     <title>Edulink</title>
 
     <!--CSS-->
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
 
-    {{--
-    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" /> --}}
+    
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
 
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
