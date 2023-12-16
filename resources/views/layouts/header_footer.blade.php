459fec26143c5786b1e0df4727db4c2542a1e44a
Chelsa Rachel Wibowo <144706070+ChelsaRachel@users.noreply.github.com>
2023-12-16 12:05:41 +0700

    *footer

--- resources/views/layouts/header_footer.blade.php
+++ resources/views/layouts/header_footer.blade.php
@@ -345,38 +345,38 @@
                             <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                                 <div>
                                     <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">
-                                        Resources</h2>
+                                        Edulink</h2>
                                     <ul class="text-white font-medium">
                                         <li class="mb-4">
-                                            <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
+                                            <a href="#" class="hover:underline">Tentang</a>
                                         </li>
                                         <li>
-                                            <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
+                                            <a href="#" class="hover:underline">Alumni</a>
                                         </li>
                                     </ul>
                                 </div>
                                 <div>
                                     <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">
-                                        Follow us</h2>
+                                        Program Kami</h2>
                                     <ul class="text-white font-medium">
                                         <li class="mb-4">
-                                            <a href="https://github.com/themesberg/flowbite"
-                                                class="hover:underline ">Github</a>
+                                            <a href="#"
+                                                class="hover:underline ">Kursus Online</a>
                                         </li>
                                         <li>
-                                            <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
+                                            <a href="#" class="hover:underline">Konsultasi Akademik</a>
                                         </li>
                                     </ul>
                                 </div>
                                 <div>
-                                    <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Legal
+                                    <h2 class="mb-6 text-sm font-semibold text-white uppercase dark:text-white">Lainnya
                                     </h2>
                                     <ul class="text-white font-medium">
                                         <li class="mb-4">
-                                            <a href="#" class="hover:underline">Privacy Policy</a>
+                                            <a href="#" class="hover:underline">Ketentuan Privasi</a>
                                         </li>
                                         <li>
-                                            <a href="#" class="hover:underline">Terms &amp; Conditions</a>
+                                            <a href="#" class="hover:underline">Syarat dan Ketentuan</a>
                                         </li>
                                     </ul>
                                 </div>
